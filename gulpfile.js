ab379b546abd35d28bb4948b5031fbe272f10b5e
VRez-web <56605107+VRez-web@users.noreply.github.com>
2020-02-01 14:27:11 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ let gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    del=require('del'),
-    autoprefixer=require('gulp-autoprefixer');
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin');
 
 
 
 
-gulp.task('clean',async function(){
+gulp.task('clean', async function () {
     del.sync('dist')
 })
 
@@ -20,7 +21,9 @@ gulp.task('scss', function () {
         .pipe(sass({
             outputStyle: 'compressed'
         }))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({
+            overrideBrowserslist: 'last 8 version'
+        }))
         .pipe(rename({
             suffix: '.min'
         }))
@@ -29,12 +32,13 @@ gulp.task('scss', function () {
             stream: true
         }))
 });
+
 gulp.task('css', function () {
 
     return gulp.src([
             'node_modules/normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
-           
+
         ])
         .pipe(concat('_libs.scss'))
         .pipe(gulp.dest('app/scss'))
@@ -59,6 +63,7 @@ gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('script'))
+    gulp.watch('app/img/*', gulp.parallel('img'))
 
 });
 //Сервер 
@@ -74,17 +79,17 @@ gulp.task('export', function () {
     let buildHtml = gulp.src('app/*.html')
         .pipe(gulp.dest('dist'))
 
-    let BuildCss=gulp.src('app/css/**/*.css')
-    .pipe(gulp.dest('dist/css'))
+    let BuildCss = gulp.src('app/css/**/*.css')
+        .pipe(gulp.dest('dist/css'))
 
-    let BuildJs=gulp.src('app/js/**/*.js')
-    .pipe(gulp.dest('dist/js'))
+    let BuildJs = gulp.src('app/js/**/*.js')
+        .pipe(gulp.dest('dist/js'))
 
-    let BuildFonts=gulp.src('app/fonts/**/*.*')
-    .pipe(gulp.dest('dist/fonts'))
+    let BuildFonts = gulp.src('app/fonts/**/*.*')
+        .pipe(gulp.dest('dist/fonts'))
 
-    let BuildImg=gulp.src('app/img/**/*.*')
-    .pipe(gulp.dest('dist/img'))
+    let BuildImg = gulp.src('app/img/**/*.*')
+        .pipe(gulp.dest('dist/img'))
 });
 gulp.task('js', function () {
     return gulp.src([
@@ -98,7 +103,12 @@ gulp.task('js', function () {
             stream: true
         }))
 });
+//Сжимание картинок
+gulp.task('img', function () {
+  return  gulp.src('app/img/**')
+       .pipe(imagemin())
+       
+});
+gulp.task('build', gulp.series('clean', 'export'))
 
-gulp.task('build', gulp.series('clean','export'))
-
-gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('css', 'scss', 'js','img', 'browser-sync', 'watch'));
